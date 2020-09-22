2c090ee3bc1b625b1cd710b4460d41ada47591e1
Chibariyooh <62876107+Chibariyooh@users.noreply.github.com>
2020-09-22 17:31:54 +0900

    mypage機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show]
-    before_action :authenticate_user!, only: [:mypage]
+    before_action :set_user, only: [:show, :edit, :update]
+    before_action :authenticate_user!, only: [:mypage, :edit, :update]
 
-
+    def index
+        @users = User.all
+    end
     def show
+    end
+
+    def edit
+        unless @user == current_user
+            redirect_to user_path(@user)
+        end
+    end
+
+    def update
+        if current_user.update(user_params)
+            redirect_to user_path(current_user)
+        else
+            redirect_to edit_user_path(current_user)
+        end
     end
 
     def mypage
@@ -12,8 +28,12 @@ class UsersController < ApplicationController
     end
 
     private
+    
         def set_user
             @user = User.find(params[:id])
         end
-    
+        
+            def user_params
+                params.fetch(:user,{}).permit(:username)
+            end
 end
